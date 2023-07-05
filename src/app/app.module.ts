73d482091f7dbcf0b0eb222710b056280633e5a0
ErikaSFS <dossantos.erika08@gmail.com>
2023-07-05 16:59:43 -0300

    2 files modified, 3 added create end-form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from '../hello.component';
@@ -8,6 +8,9 @@ import { AppService } from './app.service';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { EndFormComponent } from './end-form/end-form.component';
 
 @NgModule({
   imports: [
@@ -16,8 +19,11 @@ import { HttpClientModule } from '@angular/common/http';
     FormsModule,
     MatExpansionModule,
     HttpClientModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatCardModule,
   ],
-  declarations: [AppComponent, HelloComponent],
+  declarations: [AppComponent, HelloComponent, EndFormComponent],
   bootstrap: [AppComponent],
   providers: [AppService],
 })
---
+++ src/app/end-form/end-form.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'end-form',
+  templateUrl: './end-form.component.html',
+  stylesUrls: ['./end-form.component.css'],
+})
+export class EndFormComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
